33ddd1f74257d6e02502412d06bc066d456eb82d
tom galloway <galloway118@icloud.com>
2020-01-13 17:40:47 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Router} from '@reach/router';
+import Navbar from './Components/navBar/navBar';
+import SideBar from './Components/sideBar/sideBar';
+import WelcomePage from './Components/welcomePage/welcomePage';
+import Articles from './Components/Articles/Articles';
+import Login from './Components/Login/Login';
+import Error from './Components/Error/errorPage';
+import Header from './Components/header/header';
+import SingleArticle from './Components/singleArticle/singleArticle'
 
-function App() {
+
+class App extends React.Component {
+  state = { 
+    sideBarOpen: false,
+    user: "Tom"
+  };
+  render() {
+    let sideBar;
+    if(this.state.sideBarOpen) {
+      sideBar = <SideBar/>;
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="fixed">
+        <Header className="header" user={this.state.user}/>
+        <Navbar navBarClickHandler={this.sideBarHandler}/>
+        {sideBar}
+        </div>
+        <div className="box">
+      <Router>  
+      <WelcomePage path="/"/> 
+      <Articles path="/Articles"/>
+      <SingleArticle path="/Articles/:Article_id"/>
+      <Login path="/Login"/>
+      <Error default />
+      </Router>
+      </div>
     </div>
   );
+  }
+
+  sideBarHandler = () => {
+    this.setState((prevState) => {
+        return {sideBarOpen: !prevState.sideBarOpen};
+    })
+  };
+
 }
 
 export default App;
