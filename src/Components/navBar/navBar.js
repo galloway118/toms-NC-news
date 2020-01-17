c40069b809f05d53bdbb89e43206d5e59d73486f
tom galloway <galloway118@icloud.com>
2020-01-17 10:28:16 +0000

    login added to navbar in mobile mode

--- src/Components/navBar/navBar.js
+++ src/Components/navBar/navBar.js
@@ -10,9 +10,11 @@ const Navbar = ({navBarClickHandler, username, handleLogOut}) => {
         <nav className="navbar_links">
             <div className="navbar_toggle">
                  <Sidebutton click={navBarClickHandler}/>
-            </div>
-            {/* <img className="tomglogo" src="https://media.licdn.com/dms/image/C5603AQHz6b92YS5oiQ/profile-displayphoto-shrink_200_200/0?e=1583366400&v=beta&t=XXmAMwLi8vDmprq4JrHC9QDVZc99ImxJjCqcQH2BCJM" alt="tom g at northcoders"></img> */}
-                <div className="spacer"></div>
+            </div>    
+            {(username !== null)? 
+            <img className="tomglogo" src="https://media.licdn.com/dms/image/C5603AQHz6b92YS5oiQ/profile-displayphoto-shrink_200_200/0?e=1583366400&v=beta&t=XXmAMwLi8vDmprq4JrHC9QDVZc99ImxJjCqcQH2BCJM" alt="user"></img>:
+            <p> no user</p> }
+            <div className="spacer"></div>
                 <div className="navbar_items">
                 <ul>
                     <li><Link to="/">Home</Link></li>
